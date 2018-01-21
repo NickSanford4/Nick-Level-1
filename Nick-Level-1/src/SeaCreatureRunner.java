df8c985e7551d5cb02d1dba4f6718d38be4146fc
NickSanford4 <nicksanford4@gmail.com>
2018-01-20 16:08:58 -0800

    Typing Tutor

---
+++ Nick-Level-1/src/SeaCreatureRunner.java
@@ -0,0 +1,14 @@
+
+public class SeaCreatureRunner {
+public static void main(String[] args) {
+	SeaCreature spongebob = new SeaCreature("spongebob");
+	spongebob.eat();
+	spongebob.laugh();
+	SeaCreature patric = new SeaCreature("patrick");
+	patric.eat();
+	patric.laugh();
+	SeaCreature squidward = new SeaCreature("squidward");
+	squidward.eat();
+	squidward.laugh();
+}
+}
