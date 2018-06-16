27160856f9a7053bd7661cd8fc5ec1a33f84201c
League <league@WTS8.attlocal.net>
2018-06-16 11:31:21 -0700

    IntroToStatic

---
+++ src/Athlete.java
@@ -0,0 +1,30 @@
+public class Athlete {
+
+	     static int count = 0;
+	     static String location = "New York";
+	     static String startTime = "9.00am";
+
+	     String name;
+	     int speed;
+	     int bibNumber;
+
+	Athlete (String name, int speed){
+	     this.name = name;
+	     this.speed = speed;
+	     count++;
+	     this.bibNumber=count; 
+	}
+
+	public static void main(String[] args) {
+	     Athlete john = new Athlete("John", 10);
+	     Athlete sarah = new Athlete("Sarah", 11);
+
+	     System.out.println(sarah.name + "'s bib number is " +sarah.bibNumber);
+	     System.out.println("Total number of athletes is "+count);
+	     location = "San Diego";
+	     System.out.println(john.name+" is running in "+ john.location);
+	     john.location = "Chicago";
+	     System.out.println(sarah.name + " is running in " + sarah.location);
+	     System.out.println("The run starts at " + startTime);
+	}
+	}
